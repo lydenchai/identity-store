c84ed64c6b8d1c0c9f547b8e5b203ee8c0450828
Lyden CHAI <lyden.chai@gmail.com>
2024-01-29 21:32:30 +0700

    listing product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'identity-store';
+  products: any;
+  charts: any[] = [];
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((response) => {
+      this.products = response;
+      console.log(this.products);
+    });
+  }
+
+  onAddToChart(product: any) {
+    this.charts.push(product);
+    console.log(product);
+  }
 }
